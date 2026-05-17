82f2bdec617e08c6ca0ffb4a15ff1905cfc8d41b
harsh1214 <harsh.yadav.1412@gmail.com>
2026-05-17 22:55:24 +0530

    Update 09

--- server/src/controllers/AuthController.js
+++ server/src/controllers/AuthController.js
@@ -97,13 +97,18 @@ const login = async (req, res) => {
 }
 
 const logout = async (req, res) => {
-    res.cookie("jwt", "", {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
-        expires: new Date(0),
-    });
-    res.status(200).json({ status: "success", message: "Successfully logged out" });
+    try {
+        res.clearCookie("jwt", "", {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "none",
+        });
+        res.status(200).json({ status: "success", message: "Successfully logged out" });
+    }
+    catch (err) {
+        console.log(err);
+        return res.status(500).json({ error: "Server error" });
+    }
 }
 
 const me = async (req, res) => {
